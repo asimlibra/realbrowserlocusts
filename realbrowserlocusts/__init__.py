4afbdcc3bb1f0f2cb87cf6114f69051eade2c6e0
asimlibra <45575153+asimlibra@users.noreply.github.com>
2019-09-03 12:32:30 +0500

    Update __init__.py

--- realbrowserlocusts/__init__.py
+++ realbrowserlocusts/__init__.py
@@ -1,13 +1,14 @@
 # pylint:disable=undefined-all-variable
 """ Expose RealBrowserLocust subclasses at package level """
 from realbrowserlocusts.locusts import FirefoxLocust, PhantomJSLocust, \
-    ChromeLocust, HeadlessChromeLocust
+    ChromeLocust, HeadlessChromeLocust, HeadlessFirefoxLocust
 
 __all__ = [
     'FirefoxLocust',
     'PhantomJSLocust',
     'ChromeLocust',
     'HeadlessChromeLocust'
+    'HeadlessFirefoxLocust'
 ]
 
 __version__ = "0.2"
